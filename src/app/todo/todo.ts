fdedda6e253e9b751fd72144a5b03dbd2dfc66eb
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-08 14:37:58 +0900

    覆蓋遠端版本

--- src/app/todo/todo.ts
+++ src/app/todo/todo.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './todo.html',
-  styleUrl: './todo.css'
+  styleUrls: ['./todo.css'],
 })
 export class Todo {
+  newTask = '';
+  tasks: { name: string; completed: boolean }[] = [];
 
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push({ name: this.newTask, completed: false });
+      this.newTask = '';
+    }
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
