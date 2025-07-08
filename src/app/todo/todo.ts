441a89513722d324126c7243002c2c70d8fdbd62
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-08 14:44:26 +0900

    Setp8.修改todo.ts

--- src/app/todo/todo.ts
+++ src/app/todo/todo.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -9,18 +9,31 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './todo.html',
   styleUrls: ['./todo.css'],
 })
-export class Todo {
+export class Todo implements OnInit {
   newTask = '';
   tasks: { name: string; completed: boolean }[] = [];
+
+  ngOnInit(): void {
+    const data = localStorage.getItem('myTasks');
+    if (data) {
+      this.tasks = JSON.parse(data);
+    }
+  }
 
   addTask() {
     if (this.newTask.trim()) {
       this.tasks.push({ name: this.newTask, completed: false });
       this.newTask = '';
+      this.saveTasks();
     }
   }
 
   deleteTask(index: number) {
     this.tasks.splice(index, 1);
+    this.saveTasks();
+  }
+
+  saveTasks() {
+    localStorage.setItem('myTasks', JSON.stringify(this.tasks));
   }
 }
